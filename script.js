c5b8ccf1d94ff7573d48804c0040b9698ebb36dc
codehead-thenoncoder <user17lab@gmail.com>
2024-11-01 15:04:25 +0530

    script.js

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ document.querySelectorAll('nav a').forEach(anchor => {
         });
     });
 });
+
+// Intersection Observer for scrolling animations
+const sections = document.querySelectorAll(".parallax-section");
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("visible");
+            observer.unobserve(entry.target);
+        }
+    });
+}, {
+    threshold: 0.1
+});
+
+sections.forEach(section => {
+    observer.observe(section);
+});
